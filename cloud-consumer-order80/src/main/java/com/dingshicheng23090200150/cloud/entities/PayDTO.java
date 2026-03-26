07f0b1a1c90d8efa907cf5149bd527119400b958
Martin <ddmart1n@outlook.com>
2026-03-26 22:45:26 +0800

    添加 订单模块 main80  RestTemplateConfig paydto

---
+++ cloud-consumer-order80/src/main/java/com/dingshicheng23090200150/cloud/entities/PayDTO.java
@@ -0,0 +1,18 @@
+package com.dingshicheng23090200150.cloud.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PayDTO implements Serializable {
+    private Integer id;
+    private String payNo;
+    private String orderNo;
+    private Integer userId;
+    private BigDecimal amount;
+}
