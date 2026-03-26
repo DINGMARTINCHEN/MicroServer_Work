539bddbb662c6f8e70909d236633c5b0f60f9ce7
Martin <ddmart1n@outlook.com>
2026-03-26 22:14:53 +0800

    添加Entity

---
+++ cloud-provider-payment8001/src/main/java/entities/PayDTO.java
@@ -0,0 +1,25 @@
+package entities;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Schema(title = "支付交易DTO")
+public class PayDTO implements Serializable {
+    private Integer id;
+    @Schema(title = "支付流水号")
+    private String payNo;
+    @Schema(title = "订单流水号")
+    private String orderNo;
+    @Schema(title = "用户账号ID")
+    private Integer userId;
+    @Schema(title = "交易金额")
+    private BigDecimal amount;
+}
