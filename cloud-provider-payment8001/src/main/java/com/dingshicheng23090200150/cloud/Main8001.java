6dc0053bd210db4479f1b098bb000606ba6cb49f
Martin <ddmart1n@outlook.com>
2026-04-10 17:10:23 +0800

    修改主启动类 使用@EnableDiscoveryClient 注解，开启服务注册与发现

--- cloud-provider-payment8001/src/main/java/com/dingshicheng23090200150/cloud/Main8001.java
+++ cloud-provider-payment8001/src/main/java/com/dingshicheng23090200150/cloud/Main8001.java
@@ -2,12 +2,15 @@ package com.dingshicheng23090200150.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import tk.mybatis.spring.annotation.MapperScan;
-
 @SpringBootApplication
-@MapperScan("com.dingshicheng23090200150.cloud.mapper")
-public class Main8001 {
-    public static void main(String[] args) {
-        SpringApplication.run(Main8001.class, args);
+@MapperScan("com.atguigu.cloud.mapper") //import tk.mybatis.spring.annotation.MapperScan;
+@EnableDiscoveryClient
+public class Main8001
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Main8001.class,args);
     }
 }
