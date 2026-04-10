b440095362ea0040621935a90eb96321b2aabff4
Martin <ddmart1n@outlook.com>
2026-04-10 17:12:46 +0800

    修改 cloud-consumer-order80 消费者 80 服务 修改主启动类 添加 @EnableDiscoveryClient 注解，开启服务发现

--- cloud-consumer-order80/src/main/java/com/dingshicheng23090200150/cloud/Main80.java
+++ cloud-consumer-order80/src/main/java/com/dingshicheng23090200150/cloud/Main80.java
@@ -2,10 +2,13 @@ package com.dingshicheng23090200150.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
-public class Main80 {
-    public static void main(String[] args) {
-        SpringApplication.run(Main80.class, args);
+@EnableDiscoveryClient //该注解用于向使用consul为注册中心时注册服务
+public class Main80
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Main80.class,args);
     }
 }
