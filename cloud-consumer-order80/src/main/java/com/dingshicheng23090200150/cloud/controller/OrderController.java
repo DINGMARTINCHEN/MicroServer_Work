221e32d056546f4b4a144f901beb15950a124c6e
Martin <ddmart1n@outlook.com>
2026-04-16 17:39:42 +0800

    4.4.2 修改 cloud-consumer-order80 消费者 80 服务 修改 controller

--- cloud-consumer-order80/src/main/java/com/dingshicheng23090200150/cloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/dingshicheng23090200150/cloud/controller/OrderController.java
@@ -1,11 +1,10 @@
 package com.dingshicheng23090200150.cloud.controller;
 
-import com.dingshicheng23090200150.cloud.entities.Pay;
-import com.dingshicheng23090200150.cloud.resp.ResultData;
+import com.dingshicheng23090200150.cloud.entities.PayDTO;
+import com.dingshicheng23090200150.cloudapicommons.resp.ResultData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
-
 @RestController
 public class OrderController
 {
@@ -15,19 +14,19 @@ public class OrderController
     private RestTemplate restTemplate;
     /**
      * 一般情况下，通过浏览器的地址栏输入url，发送的只能是get请求
-     * 我们模拟消费者发送get请求，but底层调用post方法，客户端消费者参数Pay可以不添加@RequestBody
-     * @param pay
+     * 我们模拟消费者发送get请求，but底层调用post方法，客户端消费者参数PayDTO可以不添加@RequestBody
+     * @param payDTO
      * @return
      */
     @GetMapping("/consumer/pay/add")
-    public ResultData addOrder(Pay pay)
+    public ResultData addOrder(PayDTO payDTO)
     {
-        return restTemplate.postForObject(PaymentSrv_URL + "/pay/add", pay, ResultData.class);
+        return restTemplate.postForObject(PaymentSrv_URL + "/pay/add",payDTO,ResultData.class);
     }
     // 删除+修改操作作为家庭作业，O(∩_∩)O。。。。。。。
     @GetMapping("/consumer/pay/get/{id}")
     public ResultData getPayInfo(@PathVariable Integer id)
     {
-        return restTemplate.getForObject(PaymentSrv_URL + "/pay/get/" + id, ResultData.class);
+        return restTemplate.getForObject(PaymentSrv_URL + "/pay/get/"+id, ResultData.class, id);
     }
 }
