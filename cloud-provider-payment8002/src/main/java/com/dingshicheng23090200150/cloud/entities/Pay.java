48c4c61479a4401e5900f8ef101e7d2de8a15bdc
Martin <ddmart1n@outlook.com>
2026-05-08 15:08:35 +0800

    添加8002

---
+++ cloud-provider-payment8002/src/main/java/com/dingshicheng23090200150/cloud/entities/Pay.java
@@ -0,0 +1,50 @@
+package com.dingshicheng23090200150.cloud.entities;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Table(name = "t_pay")
+@Data
+@ToString
+@Schema(title = "支付交易表Entity")
+public class Pay {
+    @Id
+    @GeneratedValue(generator = "JDBC")
+    private Integer id;
+
+    @Column(name = "pay_no")
+    @Schema(title = "支付流水号")
+    private String payNo;
+
+    @Column(name = "order_no")
+    @Schema(title = "订单流水号")
+    private String orderNo;
+
+    @Column(name = "user_id")
+    @Schema(title = "用户账号ID")
+    private Integer userId;
+
+    @Column(name = "amount")
+    @Schema(title = "交易金额")
+    private BigDecimal amount;
+
+    @Column(name = "deleted")
+    @Schema(title = "删除标志,0不删1删")
+    private Byte deleted;
+
+    @Column(name = "create_time")
+    @Schema(title = "创建时间")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date createTime;
+
+    @Column(name = "update_time")
+    @Schema(title = "更新时间")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date updateTime;
+}
---
+++ cloud-provider-payment8002/src/main/java/com/dingshicheng23090200150/cloud/mapper/PayMapper.java
@@ -0,0 +1,7 @@
+package com.dingshicheng23090200150.cloud.mapper;
+
+import com.dingshicheng23090200150.cloud.entities.Pay;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface PayMapper extends Mapper<Pay> {
+}
