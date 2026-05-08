e887b1cc793e37299023ce88b397a11bf9bbac57
Martin <ddmart1n@outlook.com>
2026-05-08 14:28:58 +0800

    开始作业三

--- cloud-consumer-order80/src/main/java/com/dingshicheng23090200150/cloud/Main80.java
+++ cloud-consumer-order80/src/main/java/com/dingshicheng23090200150/cloud/Main80.java
@@ -3,7 +3,13 @@ package com.dingshicheng23090200150.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-@SpringBootApplication
+@SpringBootApplication(
+    scanBasePackages = {"com.dingshicheng23090200150.cloud"},
+    exclude = {
+        org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
+        org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration.class
+    }
+)
 @EnableDiscoveryClient //该注解用于向使用consul为注册中心时注册服务
 public class Main80
 {
