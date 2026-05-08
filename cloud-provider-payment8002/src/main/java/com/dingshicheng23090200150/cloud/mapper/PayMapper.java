4a4240a2d68fe027e6bee88128a4fa69a7aa214d
Martin <ddmart1n@outlook.com>
2026-05-08 15:29:31 +0800

    完成作业3——2

--- cloud-provider-payment8002/src/main/java/com/dingshicheng23090200150/cloud/mapper/PayMapper.java
+++
@@ -1,7 +0,0 @@
-package com.dingshicheng23090200150.cloud.mapper;
-
-import com.dingshicheng23090200150.cloud.entities.Pay;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface PayMapper extends Mapper<Pay> {
-}
